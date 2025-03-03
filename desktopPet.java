86e04a492cfb9738e2fb76ce7b9880c4b44609c9
Naru420 <120468292+Naru420@users.noreply.github.com>
2025-03-03 09:35:05 +0100

    yiwhsjf

--- desktopPet.java
+++ desktopPet.java
@@ -1,15 +1,31 @@
 import javax.swing.*;
-//import java.awt.*;
+import java.awt.*;
+
 
 
 
 public class desktopPet{
-    private JFrame f;
+   // private JFrame f;
+   Container c;
+    BackgroundPanel pet;
 
     public desktopPet() {
-    
+        c = getContentPane();
+       // setUndecorated(true);
+	//	setBackground(new Color(0,0,0,0));
+
+        pet = new BackgroundPanel(image("tempcat.png"));
+
+        setSize(260, 260);
+		setLocation(650, 350);
+        /* 
         f = new JFrame();
-       // f.isUndecorated();
-       f.setSize(20, 30);
+        f.isUndecorated();
+       f.setSize(200, 300);
+       f.setVisible(true);
+       f.setBackground(null);
+       */
+
+      
     }
 }

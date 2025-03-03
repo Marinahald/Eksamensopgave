8e96af9c519a6b2bbabe60eef36e93ece35318a6
Naru420 <120468292+Naru420@users.noreply.github.com>
2025-03-03 10:00:08 +0100

    dfghj

--- desktopPet.java
+++ desktopPet.java
@@ -1,20 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 
 
 
-public class desktopPet{
+public class desktopPet extends JFrame{
    // private JFrame f;
-   Container c;
+   
+    Container c;
     BackgroundPanel pet;
 
     public desktopPet() {
         c = getContentPane();
-       // setUndecorated(true);
-	//	setBackground(new Color(0,0,0,0));
+        setUndecorated(true);
+		setBackground(new Color(0,0,0,0));
 
-        pet = new BackgroundPanel(image("tempcat.png"));
+        pet = new BackgroundPanel(new ImageIcon("tempcat.png"));
 
         setSize(260, 260);
 		setLocation(650, 350);
@@ -25,7 +30,30 @@ public class desktopPet{
        f.setVisible(true);
        f.setBackground(null);
        */
-
+      c.add(pet);
       
     }
+
+    class BackgroundPanel extends JPanel{
+		ImageIcon icon;
+		
+		public BackgroundPanel(ImageIcon icon) {
+			this.icon = icon;
+		}
+
+		public void setIcon(ImageIcon icon) {
+			this.icon = icon;
+		}
+
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			if (this.icon != null) {
+				g.drawImage(icon.getImage(),0,0,this);
+			}
+			
+			
+		}
+		
+		
+	}
 }

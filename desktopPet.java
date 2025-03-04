c230402549246a9d561fe97dc4420e94da37491a
Marinahald <159538749+Marinahald@users.noreply.github.com>
2025-03-04 09:44:40 +0100

    asdfg

--- desktopPet.java
+++ desktopPet.java
@@ -1,9 +1,5 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import javax.swing.*;
 
 
 
